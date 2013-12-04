bbe610f0c827c706a8c2582db288ced03d82ae12
aurelle Meless <aurelle.meless@gmail.com>
2013-12-04 17:11:03 +0000

    Update function.gethtmlimage.php
    adding default image

--- plugins/tpl/common/function.gethtmlimage.php
+++ plugins/tpl/common/function.gethtmlimage.php
@@ -1,8 +1,13 @@
 <?php
 
-function jtpl_function_common_gethtmlimage($tpl, $html,$pos=0)
+function jtpl_function_common_gethtmlimage($tpl, $html,$pos=0, $default='')
 {
     preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/Ui', $html, $matches);
-    echo $matches[1][$pos];
+    if(!empty($matches[1][$pos])){
+        echo $matches[1][$pos];
+    }else{
+        echo $default;
+    }
+    
 }
 ?>
